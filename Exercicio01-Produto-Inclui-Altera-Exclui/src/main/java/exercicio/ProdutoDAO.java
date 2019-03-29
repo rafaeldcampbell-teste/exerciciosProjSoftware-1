9976abcee9d53a5c0d1075dadab6a815834e08dd
rafaeldcampbell <rafaelcampbell@id.uff.br>
2019-03-29 12:41:35 -0300

    adicionando projeto do google classroom

---
+++ Exercicio01-Produto-Inclui-Altera-Exclui/src/main/java/exercicio/ProdutoDAO.java
@@ -0,0 +1,20 @@
+package exercicio;
+
+import java.util.List;
+
+
+public interface ProdutoDAO
+{	
+	public long inclui(Produto umProduto); 
+
+	public void altera(Produto umProduto)
+		throws ProdutoNaoEncontradoException; 
+	
+	public void exclui(long id) 
+		throws ProdutoNaoEncontradoException; 
+	
+	public Produto recuperaUmProduto(long numero) 
+		throws ProdutoNaoEncontradoException; 
+	
+	public List<Produto> recuperaProdutos();
+}
